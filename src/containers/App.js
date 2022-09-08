acdde9a955a63b9dabc4703b712fb821ad7fa1b4
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-09-09 00:38:42 +0100

    use redux to manage state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,39 @@
-import React, {useState} from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
-import { robots } from "../robots";
 import Scroll from "../components/Scroll";
 import './App.css'
+import { setSearchField, requestRobots  } from "../actions";
 
-const App = () => {
-  const [searchField, setSearchField] = useState('')
-  const filteredRobots  = robots.filter(robot => robot.name.toLowerCase().includes(searchField))
-  const searchChange = (event) => {
-    setSearchField(event.target.value)
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error,
   }
+}
 
-  return (
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    fetchRobots: () => dispatch(requestRobots())
+  }
+}
+
+const App = (props) => {
+  const { searchField, onSearchChange, robots, isPending, fetchRobots } = props
+  const filteredRobots  = robots.filter(robot => robot.name.toLowerCase().includes(searchField))
+
+  useEffect(() => {
+    fetchRobots()
+  }, [fetchRobots])
+
+  return isPending ? <h1>Loading</h1> : (
     <div className="tc">
       <h1 className="f1">RoboFriends</h1>
-      <SearchBox searchChange={searchChange}/>
+      <SearchBox searchChange={onSearchChange}/>
       <Scroll>
         <CardList robots={filteredRobots} />
       </Scroll>
@@ -23,4 +41,4 @@ const App = () => {
   )
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 import App from './containers/App';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger()
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
