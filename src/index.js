6b38fc08ff19deb7e3fb0c9d2129fa67daf1d698
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-10-01 21:18:43 +0100

    initiate setrvice worker support

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'tachyons'
 import App from './containers/App';
 import { searchRobots, requestRobots } from './reducers';
@@ -20,6 +21,11 @@ root.render(
     <App />
   </Provider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
